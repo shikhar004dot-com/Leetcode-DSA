be75ba00dd85daa462cb10b20cef13e03f466380
Shikhar <shikharsrivastava816@gmail.com>
2026-06-28 11:17:25 +0530

    Time: 9 ms (67.5%), Space: 77.6 MB (41.25%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -5,7 +5,7 @@ class Solution {
             arr[0]=1;
         }
         for(int i=1;i<arr.length;i++){
-            if(Math.abs(arr[i]-arr[i-1])>1){
+            if(arr[i]-arr[i-1]>1){
                 arr[i]=arr[i-1]+1;
             }
         }
